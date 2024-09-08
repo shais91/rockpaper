371d7cbd7918034f06ea8aee1e864cac671d5093
shais91 <muhammadshais1991@gmail.com>
2024-09-09 01:17:58 +0500

    too sleepy to write something

--- java/javascript.js
+++ java/javascript.js
@@ -11,34 +11,60 @@
 let computerchoice = function getComputerChoice() {
   let mathrandom = Math.floor(Math.random() * 3) + 1;
   if (mathrandom === 1) {
-    return "Rock";
-  } else if (mathrandom === 2) {
+    return "rock";
+  }
+  if (mathrandom === 2) {
     return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 };
 console.log(computerchoice());
 
 let humanchoice = function getHumanChoice() {
   let humanPrompt = prompt("Enter 'rock' 'paper' or 'scissors'").toLowerCase();
-  if (humanPrompt === "rock") {
-    return "rock";
-  }
-  if (humanPrompt === "paper") {
-    return "paper";
-  }
-  if (humanPrompt === "scissors") {
-    return "scissors";
+  if (
+    humanPrompt === "rock" ||
+    humanPrompt === "paper" ||
+    humanPrompt === "scissors"
+  ) {
+    return humanPrompt;
   } else {
     return "Please enter valid value for rock paper or scissors";
   }
 };
-console.log(humanchoice());
+// console.log(humanchoice());
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playround(humanChoice, computerChoice) {}
-const humanSelection = humanchoice;
-const computerSelection = computerchoice;
+function playround(humanChoice, computerChoice) {
+  if (humanChoice === "Please enter valid value for rock paper or scissors") {
+    return "Please enter valid value for rock paper or scissors";
+  }
+  if (humanChoice === computerChoice) {
+    return "Draw Try again";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    return "You win";
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
+    return "Computer wins";
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    return "You win";
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
+    return "Computer wins";
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    return "You win";
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
+    return "Computer wins";
+  }
+}
+// console.log();
+const humanSelection = humanchoice();
+const computerSelection = computerchoice();
+console.log(playround(humanSelection, computerSelection));
